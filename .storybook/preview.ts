e5f2d43dbf239769c81f4cc89b5c5240e2f81c54
demagab <64416441+demagab@users.noreply.github.com>
2025-02-16 17:33:48 +0100

    PrimeNG imports

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,23 @@
-import type { Preview } from "@storybook/angular";
+import { applicationConfig, type Preview } from "@storybook/angular";
 import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
+import '!style-loader!css-loader!sass-loader!../src/styles.scss';
+import { inject, provideAppInitializer } from "@angular/core";
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { PrimeNG } from "primeng/config";
+import Aura from '@primeng/themes/aura';
+
 setCompodocJson(docJson);
+
+function provideTheme(): void {
+  const config = inject(PrimeNG);
+  config.theme.set({
+    preset: Aura,
+    options: {
+      darkModeSelector: false,
+    },
+  });
+}
 
 const preview: Preview = {
   parameters: {
@@ -12,6 +28,14 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    applicationConfig({
+      providers: [
+        provideAnimations(),
+        provideAppInitializer(provideTheme),
+      ],
+    }),
+  ],
 };
 
 export default preview;
